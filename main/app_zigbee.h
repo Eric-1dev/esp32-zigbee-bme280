65273c8cbc2db4b21550525f049f748a3dcf8165
Alexey Anisimov <eric-1@yandex.ru>
2025-05-11 02:16:38 +0400

    рефакторинг

--- main/app_zigbee.h
+++ main/app_zigbee.h
@@ -4,11 +4,11 @@
 #include <stdio.h>
 
 #define MANUFACTURER_NAME           "\x08""Eric Inc"
-#define MODEL_IDENTIFIER            "\x07"CONFIG_IDF_TARGET
+#define MODEL_IDENTIFIER            "\x0C""SensorBME280"
 
 #define INSTALLCODE_POLICY_ENABLE   false
 #define ED_AGING_TIMEOUT            ESP_ZB_ED_AGING_TIMEOUT_64MIN
-#define ED_KEEP_ALIVE               1000 /* 1000 millisecond */
+#define ED_KEEP_ALIVE               3000 /* 1000 millisecond */
 #define HA_ESP_SENSOR_ENDPOINT      10 /* esp temperature sensor device endpoint, used for temperature measurement */
 
 typedef void (*zigbee_network_event_cb)(void);
@@ -19,7 +19,10 @@ void update_temperature_value(int16_t temperature_degrees_tenths);
 void update_humidity_value(uint16_t humidity_tenths);
 void update_pressure_value(int16_t pressure_tenths);
 void update_battery_value(uint8_t battery_percent);
-void register_zigbee_network_joined_callback(zigbee_network_event_cb cb);
+void send_device_announce(void);
+void register_factory_reset_mode_callback(zigbee_network_event_cb cb);
+void register_network_joined_callback(zigbee_network_event_cb cb);
 void register_connection_failed_callback(zigbee_network_event_cb cb);
+void register_reboot_success_callback(zigbee_network_event_cb cb);
 
 #endif
